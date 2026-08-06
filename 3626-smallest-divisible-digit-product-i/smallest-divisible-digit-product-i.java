3614b9ffc58799e7fde5dfebc41554de5c187041
ASMIT CHOWDHURY <asmit.chowdhury.2007@gmail.com>
2026-08-06 11:39:56 +0530

    Time: 1 ms (100.00%) | Memory: 43 MB (14.66%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        int result = 0;
+        while(n>0){
+            int temp =n;
+            int product =1;
+            while(temp>0){
+                int quotient = temp/10;
+                int remainder = temp%10;
+                product = product*remainder;
+                temp=quotient;
+            }
+            if(product%t==0){
+                return n;
+            }
+            n=n+1;
+        }
+        return -1;
+    }
+}
