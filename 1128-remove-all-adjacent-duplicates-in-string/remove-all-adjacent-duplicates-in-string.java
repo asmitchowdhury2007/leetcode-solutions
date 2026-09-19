7dd9d243d0c65ff038af70ff034444e89798d410
ASMIT CHOWDHURY <asmit.chowdhury.2007@gmail.com>
2026-09-19 12:54:33 +0530

    Time: 25 ms (58.87%) | Memory: 47.1 MB (62.67%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(st.isEmpty()){
+                st.push(s.charAt(i));
+            }
+            else{
+                if(st.peek()==s.charAt(i)){
+                    st.pop();
+                }
+                else{
+                    st.push(s.charAt(i));
+                }
+            }
+        }
+        StringBuilder str = new StringBuilder();
+        for(char ch : st){
+            str.append(ch);
+        }
+        String result = str.toString();
+        return result;
+    }
+}
